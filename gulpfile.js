ac80cbff8f4e39d589f0a3de9b57e0ccbccea1aa
Kelly Ripple <kelly.m.ripple@gmail.com>
2016-06-18 10:15:33 -0400

    refactor file paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+var jekyllSourcePath = 'index.html';
+var htmlSourcePath = '_site/**/*.html';
+var htmlDestinationPath = '_site/';
+
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var minifyHTML = require('gulp-minify-html');
@@ -7,16 +11,18 @@ gulp.task('default', function() {
 });
 
 gulp.task('jekyll', function() {
-  return gulp.src('index.html', { read: false })
+  return gulp.src(jekyllSourcePath, { read: false })
     .pipe(shell([
       'jekyll build'
   ]));
 });
 
 gulp.task('html', ['jekyll'], function() {
-  return gulp.src('_site/**/*.html')
+  return gulp.src(htmlSourcePath)
     .pipe(minifyHTML({
         quotes: true
     }))
-    .pipe(gulp.dest('_site/'));
+    .pipe(gulp.dest(htmlDestinationPath));
 });
+
+
