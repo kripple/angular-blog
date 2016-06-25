5ec6c577dcb5c3a75f3889be29054c6d5331e67a
Kelly Ripple <kelly.m.ripple@gmail.com>
2016-06-24 21:00:51 -0400

    update gulpfile, add todos

--- _site/gulpfile.js
+++ _site/gulpfile.js
@@ -1,38 +1,48 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var minifyHtml = require('gulp-htmlmin');
-var autoprefixer = require('gulp-autoprefixer');
-var minifyCss = require('gulp-clean-css');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var concatCss = require('gulp-concat-css');
-var minifyImg = require('gulp-imagemin');
-var download = require('gulp-download');
+/* global require */
 
-var originPath = '_site/';
-var destinationPath = '../blog-src/';
+var gulp          = require('gulp');
+var shell         = require('gulp-shell');
+var minifyHtml    = require('gulp-htmlmin');
+var autoprefixer  = require('gulp-autoprefixer');
+var minifyCss     = require('gulp-clean-css');
+var rename        = require('gulp-rename');
+var concatCss     = require('gulp-concat-css');
+var minifyImg     = require('gulp-imagemin');
+var download      = require('gulp-download');
 
-var htmlSource = '_site/**/*.html';
-var cssSource = 'public/**/*.css';
-var imgSource = 'public/imgs/**/*';
+var htmlSource    = '_site/**/*.html';
+var cssSource     = 'public/css/*.css';
+var imgSource     = 'public/imgs/*';
 
-var htmlReadPath = originPath;
-var cssReadPath = originPath + 'public/css/';
-var imgReadPath = originPath +'public/imgs/';
+var destination   = '../blog-src/';
+var htmlWrite     = destination;
+var cssWrite      = destination + 'public/css/';
+var imgWrite      = destination + 'public/imgs/';
+var jsWrite       = destination + 'public/js/';
+var clean         = '~/repos/blog-src/*';
 
-var htmlWritePath = destinationPath;
-var cssWritePath = destinationPath + 'public/css/';
-var imgWritePath = destinationPath + 'public/imgs/';
-var jsWritePath = destinationPath + 'public/js/'
+// todo:
+// optimize font loading
+// seo stuff
+// only do img, html, css if changed
+// ???
 
+
+// FIXME jekyll needs more stuff to format properly - some kind of deployment process is missing
 
 gulp.task('default', ['jekyll','html','css','images','analytics']);
 
+gulp.task('clean', function() {
+  return gulp.src(destination, { read: false })
+    .pipe(shell([
+      ( 'rm -r ' + clean )
+  ]));
+});
 
 gulp.task('jekyll', function() {
   return gulp.src('index.html', { read: false })
     .pipe(shell([
-      'jekyll build'
+      'JEKYLL_ENV=production jekyll build'
   ]));
 });
 
@@ -44,27 +54,27 @@ gulp.task('html', ['jekyll'], function() {
       sortClassName: true,
       removeComments: true
     }))
-    .pipe(gulp.dest(htmlWritePath));
+    .pipe(gulp.dest(htmlWrite));
 });
 
 gulp.task('css', ['jekyll'], function() {
   return gulp.src(cssSource)
     .pipe(autoprefixer())
-    .pipe(concatCss(cssWritePath))
+    .pipe(concatCss(cssWrite))
     .pipe(rename('style.min.css'))
-    .pipe(minifyCss({keepBreaks:false}))
-    .pipe(gulp.dest(cssWritePath));
+    .pipe(minifyCss())
+    .pipe(gulp.dest(cssWrite));
 });
 
 gulp.task('images', ['jekyll'], function () {
   return gulp.src(imgSource)
     .pipe(minifyImg())
-    .pipe(gulp.dest(imgWritePath));
+    .pipe(gulp.dest(imgWrite));
 });
 
 gulp.task('analytics', ['jekyll'], function() {
   return download('https://www.google-analytics.com/analytics.js')
-    .pipe(gulp.dest(jsWritePath));
+    .pipe(gulp.dest(jsWrite));
 });
 
 
