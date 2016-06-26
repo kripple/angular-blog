9acb2015404de1df6ecebc56fc50002268f985c5
Kelly Ripple <kelly.m.ripple@gmail.com>
2016-06-26 18:38:07 -0400

    add gulp seo task to build sitemap

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,15 @@ var rename        = require('gulp-rename');
 var concatCss     = require('gulp-concat-css');
 var minifyImg     = require('gulp-imagemin');
 var download      = require('gulp-download');
+var request       = require('request');
+var sitemap       = require('gulp-sitemap');
+var eslint        = require('gulp-eslint');
+var execSync      = require('child_process').execSync;
 
 var htmlSource    = '_site/**/*.html';
 var cssSource     = 'public/css/*.css';
 var imgSource     = 'public/imgs/*';
+var jsSource      = ['_site/**/*.js','!node_modules/**']; 
 
 var clean         = '~/repos/kripple.github.io/*';
 
@@ -24,6 +29,8 @@ var jsWrite       = destination + 'public/js/';
 
 var devConfig     = '_config.yml';
 var prodConfig    = '_config_prod.yml';
+
+var url           = 'http://kellyripple.com';
 
 // todo:
 // 1. optimize font & image loading
@@ -68,6 +75,20 @@ gulp.task('css', function(done) {
   done();
 });
 
+gulp.task('lint', function(done) {
+  return gulp.src(jsSource)
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+  done();
+});
+
 gulp.task('images', function () {
   return gulp.src(imgSource)
     .pipe(minifyImg())
@@ -89,13 +110,40 @@ gulp.task('clean', function(done) {
   done();
 });
 
-gulp.task('default', gulp.series('dev-build','html','css','images','analytics', function(done) {
+gulp.task('seo', function(done) {
+  gulp.src(htmlSource)
+  .pipe(sitemap({
+    siteUrl: url,
+    verbose: true,
+    lastmod: getDateTimeOfLatestCommit,
+    getLoc: removeFileExtension
+  }))
+  .pipe(gulp.dest(htmlWrite));
+  request('http://www.google.com/webmasters/tools/ping?sitemap={./sitemap.xml}');
+  request('http://www.bing.com/webmaster/ping.aspx?siteMap={./sitemap.xml}');
+  done();
+});
+
+gulp.task('default', gulp.series('dev-build', function(done) {
   done();
 }));
 
-gulp.task('prod', gulp.series('prod-build','html','css','images','analytics', function(done) {
+gulp.task('prod', gulp.series('clean','prod-build','html','css','images','analytics','seo', function(done) {
   done();
 }));
+
+function getDateTimeOfLatestCommit(file) {
+  var cmd = 'git log -1 --format=%cI "' + file.relative + '"';
+  var buffer = execSync(cmd, {
+      cwd: file.base
+    });
+  return buffer.toString().trim();
+}
+
+function removeFileExtension(siteUrl, loc, entry) {
+  return loc.substr(0, loc.lastIndexOf('.')) || loc; 
+}
+
 
 
 
